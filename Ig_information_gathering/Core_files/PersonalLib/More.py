dc112a3fb1333329229e18e9a54a8a16ddde7ba4
KLP-p <nolann.pnt.2109@gmail.com>
2025-04-12 21:53:24 +0200

    Add files via upload

---
+++ Ig_information_gathering/Core_files/PersonalLib/More.py
@@ -0,0 +1,7 @@
+#############################
+#############################
+
+Failed = "Something Worng"
+isData = 'window._sharedData = {"config":'
+Ghost1 = '<script type="text/javascript">window._sharedData = '
+Ghost2 = ';</script>'
